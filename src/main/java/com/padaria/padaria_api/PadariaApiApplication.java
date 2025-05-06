0525a162f63525deb48e99fac4506fc2b0700fe6
kauanMateus <kauanmpereira1422@gmail.com>
2025-05-05 22:58:07 -0300

    unversioned Files

---
+++ src/main/java/com/padaria/padaria_api/PadariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.padaria.padaria_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadariaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadariaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/padaria/padaria_api/PadariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padaria.padaria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadariaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

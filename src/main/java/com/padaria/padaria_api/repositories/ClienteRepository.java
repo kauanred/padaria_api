654eeb4e1e45056b8aeebee1d5f3497fffcfa6bc
kauanMateus <kauanmpereira1422@gmail.com>
2025-08-24 23:05:22 -0300

    atualizações services, model, controllers...

--- src/main/java/com/padaria/padaria_api/repositories/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.padaria.padaria_api.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.padaria.padaria_api.models.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
--- src/main/java/com/padaria/padaria_api/repositories/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.padaria.padaria_api.repositories;
-
-import com.padaria.padaria_api.models.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}
